7a1d47e8fdc53baaf258111f59b1b34465f0a9c1
salvatore cammarata <salvatore.cammarata04@gmail.com>
2025-09-15 13:24:04 +0200

    footer

---
+++ pages/3_report.py
@@ -0,0 +1,11 @@
+import streamlit as st
+st.title("📄 Report")
+st.write(
+ """
+ Qui verrà generato un riepilogo scaricabile dei risultati:
+ - input inseriti,
+ - percentuale ed esito,
+ - data/ora e versione del modello.
+
+ """
+)
