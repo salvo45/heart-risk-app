f3b9ab77e61ed482f96902888891969863e9f735
salvatore cammarata <salvatore.cammarata04@gmail.com>
2025-09-15 12:55:00 +0200

    scaffolding

---
+++ app.py
@@ -0,0 +1,9 @@
+import streamlit as st
+
+st.set_page_config(page_title="Heart Risk Prediction App", page_icon="❤️", layout="centered")
+
+st.title("Heart Risk Prediction App")
+st.write(" web-app didattica per stimare una percentuale di rischio cardiovascolare a partire da alcuni parametri clinici basilari.")
+
+
+
